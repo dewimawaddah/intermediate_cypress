38f1054261c0e4d3cec0c59a7ef7b3a64192444a
dewimawaddah <dewimawaddahwarrohmah@gmail.com>
2023-09-08 23:23:10 +0700

    additional fixtures in POM

--- cypress/e2e/4-web-testing/loginHerokuapp.cy.js
+++ cypress/e2e/4-web-testing/loginHerokuapp.cy.js
@@ -1,4 +1,5 @@
 import loginPage from "../../support/pageObject/loginPage";
+const user = require("../../fixtures/user.json");
 
 describe("template spec", () => {
   beforeEach(() => {
@@ -6,13 +7,13 @@ describe("template spec", () => {
     cy.visit(Cypress.env("herokuapp_url"));
   });
 
-  it("success login with valid username and password", (username, password) => {
+  it("success login with valid username and password", () => {
     cy.get("#menu-toggle > .fa").click();
     cy.get("#sidebar-wrapper").should("have.class", "active");
     cy.get(".sidebar-nav > :nth-child(4) > a").click();
     cy.get(".lead").should("have.text", "Please login to make appointment.");
-    loginPage.inputUsername("John Doe");
-    loginPage.inputPassword("ThisIsNotAPassword");
+    loginPage.inputUsername(user[0].username);
+    loginPage.inputPassword(user[0].password);
     loginPage.clickBtnLogin();
     cy.url().should("include", "/#appointment");
     cy.get("h2").should("contain.text", "Make Appointment");
